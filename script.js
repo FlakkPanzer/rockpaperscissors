6d0faa5b97f85639e99db7f9fb50b0fd345e0e20
Erjo Guce <erjo.guce214@gmail.com>
2022-05-30 05:38:51 -0400

    Added game function)

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let computerSelection = 0;
 let playerSelection = 0;
-const rpsArray = ["rock", "paper", "scissors"]
+let computerScore = 0;
+let playerScore = 0;
+const rpsArray = ["rock", "paper", "scissor"]
 
-// This function returns a random index from rpsArray
+// This function returns a random index from rpsArray.
 function computerPlay() 
 {
     // Math.floor() rounds down to the nearest whole number.
@@ -10,7 +12,68 @@ function computerPlay()
     return rpsArray[Math.floor(Math.random()*rpsArray.length)]
 }
 
-function round(playerSelection, computerSelection) 
+// This function decides who is the winner of a single round.
+function playRound(playerSelection, computerSelection) 
 {
-
+    let playerSelectionLower = playerSelection.toLowerCase();
+    if (playerSelectionLower == computerSelection)
+    {
+        return "Tie Game!";
+    }
+    else if (playerSelectionLower == "rock" && computerSelection == "paper")
+    {
+        computerScore++;
+        return "You lose! " + computerSelection + " beats " + playerSelectionLower;
+    }
+    else if (playerSelectionLower == "rock" && computerSelection == "scissor")
+    {
+        playerScore++;
+        return "You win! " + playerSelectionLower + " beats " + computerSelection;
+    }
+    else if (playerSelectionLower == "paper" && computerSelection == "rock")
+    {
+        playerScore++;
+        return "You win! " + playerSelectionLower + " beats " + computerSelection;
+    }
+    else if (playerSelectionLower == "paper" && computerSelection == "scissor")
+    {
+        computerScore++;
+        return "You lose! " + computerSelection + " beats " + playerSelectionLower;
+    }
+    else if (playerSelectionLower == "scissor" && computerSelection == "rock")
+    {
+        computerScore++;
+        return "You lose! " + computerSelection + " beats " + playerSelectionLower;
+    }
+    else if (playerSelectionLower == "scissor" && computerSelection == "paper")
+    {
+        playerScore++;
+        return "You win! " + playerSelectionLower + " beats " + computerSelection;
+    }
 }
+
+// This function will accept user input and determine the winner after 5 rounds. 
+function game()
+{
+    for (let i = 0; i < 5; i++)
+    {
+        playerSelection = window.prompt("Enter 'rock', 'paper' or scissor:");
+        playRound(playerSelection, computerPlay());
+    }
+
+    if (playerScore > computerScore)
+    {
+        console.log("Congrats! You have beaten the computer in a game of rock paper scissors");
+    }
+
+    if (playerScore < computerScore)
+    {
+        console.log("Bummer! The computer has beaten you in a game of rock paper scissors");
+    }
+
+    if (playerScore == computerScore)
+    {
+        console.log("You and the computer have tied in a game best of five. Play again!");
+    }
+}
+
