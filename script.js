afe471120650b1a631d49012dea0469946c7e3ce
Erjo Guce <erjo.guce214@gmail.com>
2022-05-30 00:33:12 -0400

    Added computerPlay() function

---
+++ script.js
@@ -0,0 +1,10 @@
+const rpsArray = ["rock", "paper", "scissors"]
+
+// This function returns a random index from rpsArray
+function computerPlay() 
+{
+    // Math.floor() rounds down to the nearest whole number.
+    //math.random() generates a random number between 0 and 1 exclusive.
+    return rpsArray[Math.floor(Math.random()*rpsArray.length)]
+}
+
